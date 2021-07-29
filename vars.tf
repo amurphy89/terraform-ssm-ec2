09b2b7f7ff12859c49741e77465d92002b6bc4d7
Ashley Murphy <amarmakuni@ashleymurphy.local>
2021-07-29 14:39:27 +0100

    comment out additional azs

--- vars.tf
+++ vars.tf
@@ -2,8 +2,8 @@ variable "subnet_azs" {
   type = list(string)
   default = [
     "eu-west-1a",
-    "eu-west-1b",
-    "eu-west-1c"
+    # "eu-west-1b",
+    # "eu-west-1c"
   ]
 }
 
@@ -11,8 +11,8 @@ variable "public_subnet_cidrs" {
   type = list(string)
   default = [
     "10.0.1.0/24",
-    "10.0.2.0/24",
-    "10.0.3.0/24"
+    # "10.0.2.0/24",
+    # "10.0.3.0/24"
   ]
 }
 
@@ -20,7 +20,7 @@ variable "private_subnet_cidrs" {
   type = list(string)
   default = [
     "10.0.4.0/24",
-    "10.0.5.0/24",
-    "10.0.6.0/24"
+    # "10.0.5.0/24",
+    # "10.0.6.0/24"
   ]
 }
